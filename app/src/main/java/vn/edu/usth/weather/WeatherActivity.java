b79dbfc31b9d32493a2e834ab3863ef164bfea8b
macorn <trungnq.b8182@st.usth.edu.vn>
2019-11-15 23:13:14 +0700

    Labwork 4

---
+++ app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -0,0 +1,49 @@
+package vn.edu.usth.weather;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class WeatherActivity extends AppCompatActivity {
+
+    private final static String TAG = "TestActivity";
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather);
+        ForecastFragment test = new ForecastFragment();
+        getSupportFragmentManager().beginTransaction().add(R.id.main_activity,test).commit();
+        Log.i(TAG, "On Create .....");
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "On Destroy .....");
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "On Pause .....");
+    }
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG, "On Restart .....");
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "On Resume .....");
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "On Start .....");
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "On Stop .....");
+    }
+}
